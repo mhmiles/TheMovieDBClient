7602f55ccafbc0aca4c3ad8a7f46c816d31555ff
Miles Hollingsworth <mhmiles@me.com>
2016-09-09 14:17:47 -0700

    Initial Commit

---
+++ TheMovieDBClient/TheMovieDBClient.h
@@ -0,0 +1,19 @@
+//
+//  TheMovieDBClient.h
+//  TheMovieDBClient
+//
+//  Created by Miles Hollingsworth on 9/9/16.
+//  Copyright © 2016 Miles Hollingsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TheMovieDBClient.
+FOUNDATION_EXPORT double TheMovieDBClientVersionNumber;
+
+//! Project version string for TheMovieDBClient.
+FOUNDATION_EXPORT const unsigned char TheMovieDBClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TheMovieDBClient/PublicHeader.h>
+
+
